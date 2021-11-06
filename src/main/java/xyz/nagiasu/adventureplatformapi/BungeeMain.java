a14fab0eb78e2d3990a5bbfe0a8d0871ce82c52f
MiunaPA <fish126732@gmai.com>
2021-11-06 10:36:21 +0800

    打包API

---
+++ src/main/java/xyz/nagiasu/adventureplatformapi/BungeeMain.java
@@ -0,0 +1,10 @@
+package xyz.nagiasu.adventureplatformapi;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class BungeeMain extends Plugin {
+    @Override
+    public void onEnable() {
+        // nothing
+    }
+}
---
+++ src/main/java/xyz/nagiasu/adventureplatformapi/SpigotMain.java
@@ -0,0 +1,10 @@
+package xyz.nagiasu.adventureplatformapi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SpigotMain extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        // nothing
+    }
+}
